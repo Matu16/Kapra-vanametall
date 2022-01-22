aaae6f5c7f839e4c2f982503222b3fc9e035d3c7
ManniCoon <63167163+Mannicoon@users.noreply.github.com>
2022-01-22 20:29:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("mob-nav");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  }
