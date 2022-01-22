fa3f98ffc18634c65e5b980d91c75bfa5ebd88f5
Matu16 <98150410+Matu16@users.noreply.github.com>
2022-01-22 21:39:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("mob-nav");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
